54fa0d12162ce98ea771f176e368c33221cdc69a
Lucas Americano <121906831+americanlucas@users.noreply.github.com>
2024-08-13 15:06:10 -0300

    monitoria "C" - Aula 01

---
+++ soma.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//inicio
+
+void main() {
+
+    //variaveis
+
+    int x = 3;
+    int y = 5;
+
+    int soma = 0;
+
+    //processamento e saida de dados
+
+    if(soma % 2 == 0) {
+        printf("\n O resultado soma eh par");
+    } else{
+        printf("\n O resultado da soma e impar");
+    }
+
+}
---
+++ teste-2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//inicio
+
+void main () {
+
+    //entrada de dados
+
+    int inteiro = 123;
+    float numero_quebrado = 143.55;
+    char caractere = 'A';
+
+    //processamento e saida de dados
+
+    printf("%d\n", inteiro);
+    printf("%f\n", numero_quebrado);
+    printf("%c\n", caractere);
+}
